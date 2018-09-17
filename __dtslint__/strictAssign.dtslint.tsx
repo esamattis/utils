49aa4a37c301d52348d38d997f68dd7042339238
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-18 00:48:13 +0300

    Add null test

--- __dtslint__/strictAssign.dtslint.tsx
+++ __dtslint__/strictAssign.dtslint.tsx
@@ -4,6 +4,7 @@ strictAssign({}, {});
 
 strictAssign({foo: 1}, {foo: 2});
 strictAssign({foo: 1}, {foo: 2}, {foo: 4});
+strictAssign({foo: 1}, undefined, {foo: 2}, null, {foo: 4}, null);
 
 // $ExpectError
 strictAssign({foo: 1}, {foo: "sdf"});
