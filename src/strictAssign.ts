d9157ac5484acbda3a0a280b3f2ba69895cae434
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-18 00:51:12 +0300

    fix nulls in strictAssign

--- src/strictAssign.ts
+++ src/strictAssign.ts
@@ -1,3 +1,6 @@
-export function strictAssign<T>(target: T, ...sources: Partial<T>[]): T {
+export function strictAssign<T>(
+    target: T,
+    ...sources: (Partial<T> | null | undefined)[]
+): T {
     return (Object.assign as any)(target, ...sources);
 }
